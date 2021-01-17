e1eab914b2dfb8f2e49e63f10bc4af75c6752d24
Vamshi Krishna <vamshikrish2706|@gmail.com>
2021-01-17 14:28:25 +0530

    first commit

---
+++ Controllers/Order.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System;
+using System.Data;
+
+namespace OrderManagementSystem.Controllers
+{
+    [ApiController]
+    public class Order : ControllerBase
+    {
+        [Route("Order")]
+        [HttpPost]
+        public void createorder([FromBody] OrderMS orderMS)
+        {
+
+            SqlConnection con = new SqlConnection("Data Source=.; Initial Catalog=OrderManagement;User ID=sa;Password=123");
+            SqlCommand cmd = new SqlCommand("sp_name", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@BuyerName", orderMS.BuyerInfo.BuyerName);
+                cmd.Parameters.AddWithValue("@Mobile", orderMS.BuyerInfo.Mobile);
+                cmd.Parameters.AddWithValue("@Email", orderMS.product);
+                cmd.Parameters.AddWithValue("@Street", orderMS.Address.Street);
+                cmd.Parameters.AddWithValue("@State", orderMS.Address.State);
+                cmd.Parameters.AddWithValue("@OrderStatus", orderMS.OrderStatus);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            
+        }
+
+        [Route("Order")]
+        [HttpPut]
+        public void updateOrder([FromBody] OrderMS orderMS)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.; Initial Catalog=OrderManagement;User ID=sa;Password=123");
+            SqlCommand cmd = new SqlCommand("sp_name", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@OrderID", orderMS.OrderID); ;
+            cmd.Parameters.AddWithValue("@BuyerName", orderMS.BuyerInfo.BuyerName);
+            cmd.Parameters.AddWithValue("@Mobile", orderMS.BuyerInfo.Mobile);
+            cmd.Parameters.AddWithValue("@product", orderMS.product);
+            cmd.Parameters.AddWithValue("@Street", orderMS.Address.Street);
+            cmd.Parameters.AddWithValue("@State", orderMS.Address.State);
+            cmd.Parameters.AddWithValue("@OrderStatus", orderMS.OrderStatus);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        [Route("Order")]
+        [HttpGet]
+        public OrderMS GetOrderData(int? id)
+        {
+            OrderMS order = new OrderMS();
+
+            SqlConnection con = new SqlConnection("Data Source=.; Initial Catalog=OrderManagement;User ID=sa;Password=123");
+                SqlCommand cmd = new SqlCommand("sp_name", con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    order.OrderID = Convert.ToInt32(rdr["OrderID"]);
+                    order.BuyerInfo.BuyerName = rdr["BuyerName"].ToString();
+                    order.BuyerInfo.Mobile = rdr["Mobile"].ToString();
+                    //order.product = ;
+                    order.Address.Street = rdr["Street"].ToString();
+                    order.Address.State = rdr["State"].ToString();
+                }
+            
+            return order;
+        }
+
+        public void DeleteOrder(int? id)
+        {
+                SqlConnection con = new SqlConnection("Data Source=.; Initial Catalog=OrderManagement;User ID=sa;Password=123");
+                SqlCommand cmd = new SqlCommand("sp_name", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id", id);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ OrderMS.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem
+{
+    public class OrderMS
+    {
+        public int OrderID { get; set; }
+        public BuyerInfo BuyerInfo { get; set; }
+        public List<Product> product { get; set; }
+        public Address Address { get; set; }
+        public string OrderStatus { get; set; }
+
+    }
+
+    public class BuyerInfo
+    {
+        public string BuyerName { get; set; }
+        public string Mobile { get; set; }
+    }
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public string Weight { get; set; }
+
+    }
+
+    public class Address
+    {
+        public string Street { get; set; }
+        public string State { get; set; }
+    }
+
+
+}
